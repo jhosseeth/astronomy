16ad28bf82ce56b25e3f97b1626e062cc85d2724
Jhoss Naranjo <djosephn@gmail.com>
2022-06-06 19:20:35 -0500

    Agregar plano

--- js/main.js
+++ js/main.js
@@ -1,23 +1,29 @@
 import * as THREE from 'https://unpkg.com/three@0.140.2/build/three.module.js'
 
+// Escena
 const scene = new THREE.Scene();
+
+// Camara
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
 camera.position.z = 3;
 
+// Renderizado
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// ------- MODELO ----------
+const geometry = new THREE.PlaneGeometry( 1, 1 );
+const material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+const plane = new THREE.Mesh( geometry, material );
+scene.add( plane );
+
+
+// Animacion
 function animate() {
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
-	cube.rotation.y += 0.1;
+	plane.rotation.x += 0.05;
 }
 
 animate();
